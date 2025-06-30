6bfb8be471761bc2e49fc2292be081b538819326
AchiGel <ar.gelashvili@gmail.com>
2025-06-30 15:46:12 +0400

    fix/stats padding
    fixed Stats padding

--- features/landing/components/composites/Stats.tsx
+++ features/landing/components/composites/Stats.tsx
@@ -3,7 +3,7 @@ import StatsUnit from "../primitives/StatsUnit"
 
 const Stats = () => {
   return (
-    <div className="max-w-[1164px] px-12">
+    <div className="max-w-[1152px] pt-60 pr-12 pb-60 pl-9">
       <div className="grid grid-cols-4 text-center">
         {statsData.map((item, index) => (
           <StatsUnit key={index} num={item.num} text={item.text} />
