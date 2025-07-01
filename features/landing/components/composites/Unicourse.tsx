49cf72c3d1fd31b518913d21f221504b27f6ba03
MtchedlishviliLevani <levan.mtchedlishvili.5@iliauni.edu.ge>
2025-07-01 18:34:46 +0400

    feature/update Unicourse component

--- features/landing/components/composites/Unicourse.tsx
+++ features/landing/components/composites/Unicourse.tsx
@@ -1,5 +1,22 @@
+import { uniCourseData } from "@features/landing/data/landingData"
+import UnicourseCard from "@features/landing/components/primitives/UnicourseCard"
 const Unicourse = () => {
-  return <div>Unicourse</div>
+  return (
+    <section className="ml-auto w-[1068px]">
+      <h2 className="font-case mb-[88px] text-center text-5xl leading-[56px] font-black text-[#EDFAFF]">
+        უნიკურსი
+      </h2>
+      <div className="grid grid-cols-3 gap-6 px-[18px_!important]">
+        {uniCourseData.map((course) => (
+          <UnicourseCard
+            key={course.id}
+            title={course.title}
+            image={course.image}
+          />
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default Unicourse
