d1008cb09e081208866df929885d8a9363682d0b
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-07-03 01:48:11 +0400

    feature/faq-header

---
+++ features/landing/components/composites/Faq.tsx
@@ -0,0 +1,18 @@
+const Faq = () => {
+  return (
+    <div className="m-auto max-w-[1068px]">
+      <div className="space-y-[16px] lg:space-y-[24px]">
+        <h2 className="text-[24px] font-black text-[#EDFAFF] lg:text-[48px]">
+          FAQ
+        </h2>
+        <p className="w-full max-w-[782px] text-[#D4D4D4]">
+          იმ შემთხვევაში, თუ პირველ გაერთიანებულ ლექციაზე დასწრება ვერ მოახერხე,
+          შეხვედრის ჩანაწერი გუგლ კალენდარში შეგიძლია იპოვო.
+        </p>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Faq
