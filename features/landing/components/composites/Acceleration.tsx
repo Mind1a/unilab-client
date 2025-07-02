cddc57759869403436fe0ea8b4f73daad55fc05a
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-07-03 00:13:28 +0400

    feature/acceleration-section-part1
    adding h2,
    adding first section with 2 links,
    adding border color in global css

--- features/landing/components/composites/Acceleration.tsx
+++ features/landing/components/composites/Acceleration.tsx
@@ -1,5 +1,25 @@
+import Link from "next/link"
+
 const Acceleration = () => {
-  return <div>Acceleration</div>
+  return (
+    <div className="mx-auto max-w-[1098px]">
+      <h2 className="font- text-center text-[48px] font-[900]">აქსელერაცია</h2>
+      <section className="flex justify-center gap-6">
+        <div className="flex h-[50px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6">
+          <Link className="inline-block text-[18px] font-[900]" href="/">
+            ციფრული მარკეტინგი
+          </Link>
+        </div>
+        <div className="flex h-[50px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-8 py-3">
+          <Link className="inline-block text-[18px] font-[900]" href="/">
+            ციფრული მარკეტინგი
+          </Link>
+        </div>
+      </section>
+      <section></section>
+      <section></section>
+    </div>
+  )
 }
 
 export default Acceleration
