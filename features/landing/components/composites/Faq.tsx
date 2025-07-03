6b776e3e4396f028ab269ff9669c0be6c39c09ab
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-07-03 23:36:25 +0400

    feature/add FAQ list to landing page
    Added a structured list of frequently asked questions to be used on the landing page. Each item includes question, answer, and unique ID.

--- features/landing/components/composites/Faq.tsx
+++ features/landing/components/composites/Faq.tsx
@@ -1,16 +1,38 @@
+"use client"
+import { Faqs } from "@features/landing/data/landingData"
+import Question from "../primitives/Question"
+import { FaqProps } from "@features/landing/types"
+import { useState } from "react"
+
 const Faq = () => {
+  const [openQuestionId, setOpenQuestionId] = useState<string | null>(null)
+
   return (
-    <div className="m-auto max-w-[1068px]">
+    <div className="m-auto max-w-[1068px] space-y-[24px] lg:space-y-[88px]">
       <div className="space-y-[16px] lg:space-y-[24px]">
         <h2 className="text-[24px] font-black text-[#EDFAFF] lg:text-[48px]">
           FAQ
         </h2>
-        <p className="w-full max-w-[782px] text-[#D4D4D4]">
-          იმ შემთხვევაში, თუ პირველ გაერთიანებულ ლექციაზე დასწრება ვერ მოახერხე,
-          შეხვედრის ჩანაწერი გუგლ კალენდარში შეგიძლია იპოვო.
+        <p className="text-align:justify w-full max-w-[782px] text-justify text-[#D4D4D4] lg:text-start">
+          თუკი უნილაბის ციფრული სამყაროს კარის შემოღებამდე კითხვის ნიშნების
+          კორიანტელი გიტრიალებს გონებაში მათი გაფანტვა ჩვენ მოგვანდე
         </p>
       </div>
-      <div></div>
+      <div>
+        {Faqs.map((faq: FaqProps) => (
+          <Question
+            key={faq.id}
+            title={faq.question}
+            id={faq.id}
+            isOpen={openQuestionId === faq.id}
+            onToggle={() =>
+              setOpenQuestionId((prev) => (prev === faq.id ? null : faq.id))
+            }
+          >
+            {faq.answer}
+          </Question>
+        ))}
+      </div>
     </div>
   )
 }
