58b8db5b99f1cee29a131a80326f989a91277ba8
Mindia Arabuli <mindia.arabuli@iliauni.edu.ge>
2025-07-04 14:11:10 +0400

    feature/prettier-format

--- features/landing/components/composites/Stats.tsx
+++ features/landing/components/composites/Stats.tsx
@@ -3,7 +3,7 @@ import StatsUnit from "../primitives/StatsUnit"
 
 const Stats = () => {
   return (
-    <div className="max-w-[1152px] pt-60 pr-12 pb-60 pl-9">
+    <div className="mx-auto max-w-[1152px] pt-60 pr-12 pb-60 pl-9">
       <div className="grid grid-cols-2 gap-6 text-center md:grid-cols-4 md:gap-[124px]">
         {statsData.map((item, index) => (
           <StatsUnit key={index} num={item.num} text={item.text} />
