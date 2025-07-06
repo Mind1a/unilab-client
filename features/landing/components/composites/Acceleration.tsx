dfaa85542a89a664dfb6e65206b1d14bed7ad587
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-07-07 01:34:55 +0400

    feature/responsive
    adding mobile view

--- features/landing/components/composites/Acceleration.tsx
+++ features/landing/components/composites/Acceleration.tsx
@@ -6,44 +6,44 @@ const Acceleration = () => {
       <h2 className="font- mb-[88px] text-center text-[48px] font-[900]">
         აქსელერაცია
       </h2>
-      <div className="flex flex-col gap-6">
-        <section className="flex justify-center gap-6">
-          <div className="flex h-[50px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 transition-all duration-500 hover:bg-[var(--color-cont-borders)]">
-            <Link className="inline-block text-[18px] font-[900]" href="/">
+      <div className="flex flex-col gap-6 md:flex-col">
+        <section className="flex flex-col items-center justify-center gap-6 md:flex-row">
+          <div className="flex max-w-[259px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 py-3 transition-all duration-500 hover:bg-[var(--color-cont-borders)]">
+            <Link className="text-[18px] font-[900]" href="/">
               ციფრული მარკეტინგი
             </Link>
           </div>
-          <div className="flex h-[50px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-8 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
             <Link className="inline-block text-[18px] font-[900]" href="/">
-              ციფრული მარკეტინგი
+              გრაფიკული დიზაინი
             </Link>
           </div>
         </section>
-        <section className="flex justify-center gap-6">
-          <div className="flex h-[50px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
-            <Link className="inline-block text-[18px] font-[900]" href="/">
+        <section className="flex flex-col items-center justify-center gap-6 md:flex-row">
+          <div className="flex max-w-[259px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+            <Link className="text-[18px] font-[900]" href="/">
               ინტერფეისის დიზაინი
             </Link>
           </div>
-          <div className="flex h-[50px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-8 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
-            <Link className="inline-block text-[18px] font-[900]" href="/">
+          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+            <Link className="text-[18px] font-[900]" href="/">
               front-end react
             </Link>
           </div>
-          <div className="flex h-[50px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-8 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
-            <Link className="inline-block text-[18px] font-[900]" href="/">
+          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+            <Link className="text-[18px] font-[900]" href="/">
               Python
             </Link>
           </div>
         </section>
-        <section className="flex justify-center gap-6">
-          <div className="flex h-[50px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
-            <Link className="inline-block text-[18px] font-[900]" href="/">
+        <section className="flex flex-col items-center justify-center gap-6 md:flex-row">
+          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+            <Link className="text-[18px] font-[900]" href="/">
               front-end (HTML, Css, js)
             </Link>
           </div>
-          <div className="flex h-[50px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-8 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
-            <Link className="inline-block text-[18px] font-[900]" href="/">
+          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+            <Link className="text-[18px] font-[900]" href="/">
               ციფრული პროექტების მართვა
             </Link>
           </div>
