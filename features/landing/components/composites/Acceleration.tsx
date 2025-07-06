d4403a8a571505b7a5ab1d218b0cc05e370333a0
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-07-07 01:37:32 +0400

    bugfix/colors
    adding hover colors with static
    removing border color from global css

--- features/landing/components/composites/Acceleration.tsx
+++ features/landing/components/composites/Acceleration.tsx
@@ -8,41 +8,41 @@ const Acceleration = () => {
       </h2>
       <div className="flex flex-col gap-6 md:flex-col">
         <section className="flex flex-col items-center justify-center gap-6 md:flex-row">
-          <div className="flex max-w-[259px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 py-3 transition-all duration-500 hover:bg-[var(--color-cont-borders)]">
+          <div className="flex max-w-[259px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 py-3 transition-all duration-500 hover:bg-[#0080ffcc]">
             <Link className="text-[18px] font-[900]" href="/">
               ციფრული მარკეტინგი
             </Link>
           </div>
-          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[#0080ffcc]">
             <Link className="inline-block text-[18px] font-[900]" href="/">
               გრაფიკული დიზაინი
             </Link>
           </div>
         </section>
         <section className="flex flex-col items-center justify-center gap-6 md:flex-row">
-          <div className="flex max-w-[259px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+          <div className="flex max-w-[259px] items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 py-3 transition-all duration-300 hover:bg-[#0080ffcc]">
             <Link className="text-[18px] font-[900]" href="/">
               ინტერფეისის დიზაინი
             </Link>
           </div>
-          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[#0080ffcc]">
             <Link className="text-[18px] font-[900]" href="/">
               front-end react
             </Link>
           </div>
-          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[#0080ffcc]">
             <Link className="text-[18px] font-[900]" href="/">
               Python
             </Link>
           </div>
         </section>
         <section className="flex flex-col items-center justify-center gap-6 md:flex-row">
-          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] bg-transparent px-6 py-3 transition-all duration-300 hover:bg-[#0080ffcc]">
             <Link className="text-[18px] font-[900]" href="/">
               front-end (HTML, Css, js)
             </Link>
           </div>
-          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[var(--color-cont-borders)]">
+          <div className="flex items-center justify-center rounded-[24px] border border-[var(--color-cont-borders)] px-6 py-3 transition-all duration-300 hover:bg-[#0080ffcc]">
             <Link className="text-[18px] font-[900]" href="/">
               ციფრული პროექტების მართვა
             </Link>
