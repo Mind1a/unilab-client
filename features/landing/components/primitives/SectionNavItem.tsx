d4cf7e09a12e44aeb52e0edea161436d61f717d0
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-08-10 20:06:08 +0400

    feature/add-scroll-offset

--- features/landing/components/primitives/SectionNavItem.tsx
+++ features/landing/components/primitives/SectionNavItem.tsx
@@ -1,20 +1,36 @@
-import Link from "next/link"
 import { SectionNavItemProps } from "@features/landing/types"
 
 const SectionNavItem = ({
   item,
   onClick,
   isActive = false,
-}: SectionNavItemProps) => (
-  <li
-    className={`cursor-pointer transition-all duration-300 ease-in-out ${
-      isActive ? "font-black" : "font-medium"
-    }`}
-  >
-    <Link href={`#${item.href}`} onClick={onClick}>
-      {item.section}
-    </Link>
-  </li>
-)
+}: SectionNavItemProps) => {
+  const handleClick = (event: React.MouseEvent) => {
+    event.preventDefault()
+
+    const target = document.getElementById(item.href)
+    if (target) {
+      const isMobile = window.innerWidth <= 768
+      const offset = isMobile ? -100 : -160
+      const marginTop =
+        target.getBoundingClientRect().top + window.scrollY + offset
+      window.scrollTo({ top: marginTop, behavior: "smooth" })
+    }
+
+    if (onClick) onClick()
+  }
+
+  return (
+    <li
+      className={`cursor-pointer transition-all duration-300 ease-in-out ${
+        isActive ? "font-black" : "font-medium"
+      }`}
+    >
+      <a href={`#${item.href}`} onClick={handleClick}>
+        {item.section}
+      </a>
+    </li>
+  )
+}
 
 export default SectionNavItem
