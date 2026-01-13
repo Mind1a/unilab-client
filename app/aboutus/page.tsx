0f8cba914eb3f68abb9dd63a187f9335316d0e40
giorgijintcharadze <giorgijintcharadze7@gmail.com>
2026-01-13 14:00:54 +0400

    add "About Us" page layout

---
+++ app/aboutus/page.tsx
@@ -0,0 +1,12 @@
+import CompanyPage from "@features/landing/components/company/CompanyPage"
+import React from "react"
+
+const Companyinfo = () => {
+  return (
+    <div>
+      <CompanyPage />
+    </div>
+  )
+}
+
+export default Companyinfo
