45918cd0fc23b9b5d5e37550bcaa3924af7c28e6
sleazy <nino.gabroshvili.4@iliauni.edu.ge>
2026-01-19 19:25:35 +0400

    updated footer link based on practiceLink in common used in about us
    - added target in practice link for footer social links
    - about us page has space around it on x axis

--- common/components/primitives/PracticeLink.tsx
+++ common/components/primitives/PracticeLink.tsx
@@ -5,15 +5,17 @@ type PracticeLinkProps = {
   title?: string
   href?: string
   className?: string
+  target?: string
 }
 
 function PracticeLink({
   title = "პრაქტიკულ პროგრამებზე",
   href = "/practice",
   className = "",
+  target = "_self",
 }: PracticeLinkProps) {
   return (
-    <Link href={href}>
+    <Link href={href} target={target}>
       <p
         className={`font-case flex cursor-pointer items-center justify-between font-[900] ${className}`}
       >
